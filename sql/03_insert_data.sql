3d3bd57c08e82cf9d2c6aedb1865d5e9b36ab5b3
DESKTOP-NVAVOMS\botez <botezatu.david91@gmail.com>
2023-01-25 22:10:46 +0100

    insert query

---
+++ sql/03_insert_data.sql
@@ -0,0 +1,14 @@
+--Inserare date de test in catalog
+INSERT INTO `catalog` (`nume_interventie`, `cost_materiale`, `pret_manopera`, `pret_total`) VALUES 
+('Igienizare profesionala completa', '150', '150', '300'),
+('Plomba', '100', '200', '300'),
+('Detartraj', '50', '100', '150'),
+('Extractie dinte', '100', '150', '250'),
+('Anestezie', '100', '100', '200'),
+('Imagistica', '100', '200', '300'),
+('Proteză mobilă elastică', '2000', '700', '2700'),
+('Implant dentar', '2000', '800', '2800'),
+('Sinus lifting lateral', '3000', '1000', '4000'),
+('Aparat ortodontic', '2000', '700', '2700'),
+('Albirea dintilor', '10', '30', '40'),
+('Tratament parodontoza', '10', '90', '100');
